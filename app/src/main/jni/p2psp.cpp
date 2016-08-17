00f954648cf9dd22253a87edba7ae51c3095ef72
cRAN-cg <guptachiranjeev.13@gmail.com>
2016-08-17 20:27:35 +0530

    Renamed hello-libs.cpp file

---
+++ app/src/main/jni/p2psp.cpp
@@ -0,0 +1,18 @@
+
+#include "peer_dbs.h"
+#include <jni.h>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_hellolibs_MainActivity_initMethod(JNIEnv *env, jobject instance) {
+    // TODO
+
+    ip::udp::endpoint node&;
+    ip::tcp::endpoint local(
+            ip::address::from_string("127.0.0.1"),
+            123
+    );
+    SayHello(local);
+    return env->NewStringUTF("Android Peer Intialised");
+
+}
